46a772c8c295f56473080d7500557f98a4f69c71
Chitransh <chitransh.saxena@calsoftinc.com-at-159728957415>
2024-04-03 20:58:10 +0530

    first commit

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ def home():
     return 'Hi this is a simple Web Application to demonstrate working of K8s!'
 
 
+@app.route('/home')
+def home():
+    return 'This is Home'
+
+
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
