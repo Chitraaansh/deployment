410da7e74602b90aa55bc4655dcf6bdc9302bd63
Chitraaansh <137933145+Chitraaansh@users.noreply.github.com>
2024-04-03 20:51:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+# Create a Flask application
+app = Flask(__name__)
+
+
+# Define a route for the homepage
+@app.route('/')
+def home():
+    return 'Hi this is a simple Web Application to demonstrate working of K8s!'
+
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
